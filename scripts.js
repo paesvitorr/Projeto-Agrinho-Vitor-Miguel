c4a4da302c2bd5b31636e464ddce4518a4f11d64
paesvitorr <143655816+paesvitorr@users.noreply.github.com>
2025-06-10 19:03:21 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,32 @@
+/* código para os botoes funcionarem */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const botoes = document.querySelectorAll(".menu button");
+
+  botoes.forEach((botao) => {
+    botao.addEventListener("click", () => {
+      const alvoId = botao.getAttribute("destino");
+      const alvo = document.getElementById(alvoId);
+      if (alvo) {
+      
+        const posicao = alvo.offsetTop;
+
+        
+        window.scrollTo({
+          top: posicao - 150,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
